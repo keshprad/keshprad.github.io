e806288d36b06abfb9fe8ce8e2324c2a55799d85
keshprad <keshav.r.pradeep@gmail.com>
2020-08-16 10:18:45 -0700

    Added content of Teams section

--- js/script.js
+++ js/script.js
@@ -107,34 +107,12 @@ const delayAnimate = async() => {
 }
 
 
-// 
-// Vertical Tabs
-// 
-function _class(name){
-    return document.getElementsByClassName(name);
-} 
-const switchTabs = () => {
-    let tabPanes = _class("tab-header")[0].getElementsByTagName("div");  
-    for(let i=0;i<tabPanes.length;i++){
-        tabPanes[i].addEventListener("click",function(){
-            _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-            tabPanes[i].classList.add("active");
-            
-            _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-            _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
-        });
-    }
-}
-
-
 
 // 
 // App
 // 
-
 const app = () => {
     navSlide();
     delayAnimate();
-    switchTabs();
 }
 app();
