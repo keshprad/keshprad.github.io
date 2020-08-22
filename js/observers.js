1b216c65ffb5fc0c47a4d1296d32d9be2a2c5e2f
keshprad <keshav.r.pradeep@gmail.com>
2020-08-22 00:14:25 -0700

    delete unused code

--- js/observers.js
+++ js/observers.js
@@ -19,26 +19,3 @@ const homeObserver = new IntersectionObserver(function(entries) {
 });
 
 homeObserver.observe(home);
-
-
-// const appearOptions = {
-//     threshold: 1, rootMargin: "0px 0px -100px 0px"
-// };
-// const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-//     entries.forEach (entry => {
-//         if (!entry.isIntersecting){
-//             return;
-//         } else {
-//             if (entry.target.classList.contains("from-right")) {
-//                 entry.target.classList.add("animate__animated animate__fadeInRight");
-//             } else if (entry.target.classList.contains("from-left")) {
-//                 entry.target.classList.add("animate__animated animate__fadeInLeft");
-//             }
-//             appearOnScroll.unobserve(entry.target);
-//         }
-//     })
-// }, appearOptions);
-
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// });
