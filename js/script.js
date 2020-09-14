948f7fe70f03191302c0bf6a3dcbb5d55a629ea2
keshprad <keshav.r.pradeep@gmail.com>
2020-09-13 19:10:03 -0700

    🖼 update profile pic!

--- js/script.js
+++ js/script.js
@@ -127,7 +127,7 @@ function enableScroll() {
 } 
 
 window.onload = async() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     await sleep(750);
     disableScroll();
     await sleep(5000);
