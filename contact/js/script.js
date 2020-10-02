964c3e960bcb6e680878e4fbb3e0f94ede286fe6
Keshav Pradeep <keshav.r.pradeep@gmail.com>
2020-10-02 09:45:53 -0700

    new contact page

---
+++ contact/js/script.js
@@ -0,0 +1,24 @@
+function goTo(ele_query, xBool, yBool) {
+  ele = $(ele_query);
+  ele_pos = ele.position();
+
+  if (xBool && yBool) {
+    window.scrollTo({
+      top: ele_pos.top,
+      left: ele_pos.left,
+      behavior: "smooth",
+    });
+  } else if (yBool) {
+    window.scrollTo({
+      top: 0,
+      left: ele_pos.left,
+      behavior: "smooth",
+    });
+  } else if (xBool) {
+    window.scrollTo({
+      top: ele_pos.top,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+}
