fe699ec40eff7516fe60576498cb2de58f7953a6
keshprad <keshav.r.pradeep@gmail.com>
2021-04-13 11:38:36 -0700

    remove superfluous animations

--- js/script.js
+++ js/script.js
@@ -99,31 +99,8 @@ function init() {
   new TypeWriter(txt, words, wait);
 }
 
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-
-//
-// temporary no scroll
-//
-function disableScroll() {
-  // Get the current page scroll position
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
-    // if any scroll is attempted, set this to the previous value
-    (window.onscroll = function () {
-      window.scrollTo(scrollLeft, scrollTop);
-    });
-}
-
-function enableScroll() {
-  window.onscroll = function () {};
-}
-
 window.onload = async () => {
   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  await sleep(750);
-  disableScroll();
-  await sleep(3000);
-  enableScroll();
 };
 
 //
