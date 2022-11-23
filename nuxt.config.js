7906244e232f4aa73dd71f0656ea224dff1d2803
keshprad <32313895+keshprad@users.noreply.github.com>
2022-11-23 14:45:51 -0500

    update resume and create redirect for better resume file name

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,8 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  serverMiddleware: ['~/middleware/redirects.js'],
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
